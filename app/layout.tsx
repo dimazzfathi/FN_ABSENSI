adc7da0887d4672e26d38c335ef7aad3b87e7ee0
bimasuluk <akunpelajaran49@gmail.com>
2024-08-13 09:59:07 +0700

    S: dimz lol 13 agst

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Login from "./tes/auth/login";
 
 const poppins = Poppins({ weight: ["300", "400", "400"], subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Login />{children}
+        {children}
         </body>
         
     </html>
