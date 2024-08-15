defb50aa0ba631660a19f4ec9e12fc18a789f0b0
bimasuluk <akunpelajaran49@gmail.com>
2024-08-15 14:38:37 +0700

    S: menambah

--- app/master_data/akademik/kelas/page.tsx
+++ app/master_data/akademik/kelas/page.tsx
@@ -1,9 +1,34 @@
-import Hal from '../kelas/hal';
+"use client";
+import React, { useState } from 'react'
+import Layout from "../../../components/layout/page"
+import Navbar from "../../../components/layout/navbar/page"
+import Footer from "../../../components/layout/footer/page"
+import Kelas from "./hal"
 
-export default function Home() {
+  const Page = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
   return (
-    <div className=" mx-auto">
-      <Hal />
-    </div>
-  );
+    <>
+    <div>
+    <Navbar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+    
+    {/* Main Content */}
+    <main className={`px-30 transition-transform duration-300 z-40 ${isOpen ? 'ml-64' : 'ml-0'}`}>
+      <div className="flex-1 p-6 ">
+          <div className="min-h-screen">
+            <Kelas />
+          </div>
+      </div>
+    </main>
+    <Footer />
+  </div>
+  </>
+  )
 }
+
+export default Page
