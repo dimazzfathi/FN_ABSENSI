5c12de09e42beba80b59abef72c771ab9a62b2f3
dimazzfathi <dimazzfathi@gmail.com>
2024-09-19 15:10:13 +0700

    D: head

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect, useRef } from 'react';
-import Navbar from './components/navbar/page';
+import Navbar from './header';
 import Pw from "../app/administrator/add_user/pass";
 
 const Page = () => {
